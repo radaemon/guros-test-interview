66a46f909538d7bfddab5e94d4f22f8493ae9e39
Alejandro Mojarro <amojarrot@gmail.com>
2021-05-05 23:47:49 -0600

    done2

---
+++ README.md
@@ -0,0 +1,9 @@
+Server:
+https://git.heroku.com/guros-app.git
+
+Endpoints:
+GET /stats
+POST /mutation
+
+Repo:
+https://github.com/mojarro92/prueba-seguros
--- hasMutation.js
+++ hasMutation.js
@@ -1,43 +1,55 @@
-
-
 function hasMutation(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[i].length; j++) {
-            if (anyAdjacentRepeatedNeighbors(array, [i, j], array[i][j]) === true) {
-                return true;
-            }
-        }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (anyAdjacentRepeatedNeighbors(array, [i, j], array[i][j]) === true) {
+        return true
+      }
     }
-    return false;
+  }
+  return false
 }
 
 function anyAdjacentRepeatedNeighbors(array, [i, j], gene) {
+  let repeatedGenes = false
 
-    let repeatedGenes = false;
+  let northEastNeighs = [
+    [i - 1, j + 1],
+    [i - 2, j + 2],
+    [i - 3, j + 3],
+  ]
+  let eastNeighs = [
+    [i, j + 1],
+    [i, j + 2],
+    [i, j + 3],
+  ]
+  let southEastNeighs = [
+    [i + 1, j + 1],
+    [i + 2, j + 2],
+    [i + 3, j + 3],
+  ]
+  let southNeighs = [
+    [i + 1, j],
+    [i + 2, j],
+    [i + 3, j],
+  ]
 
-    let northEastNeighs = [[i - 1, j + 1], [i - 2, j + 2], [i - 3, j + 3]];
-    let eastNeighs = [[i, j + 1], [i, j + 2], [i, j + 3]];
-    let southEastNeighs = [[i + 1, j + 1], [i + 2, j + 2], [i + 3, j + 3]];
-    let southNeighs = [[i + 1, j], [i + 2, j], [i + 3, j]];
+  let allGroups = [northEastNeighs, eastNeighs, southEastNeighs, southNeighs]
 
-    let allGroups = [northEastNeighs, eastNeighs, southEastNeighs, southNeighs];
-
-    allGroups.forEach((group) => {
-        let count = 0;
-        group.forEach(([i, j]) => {
-            if (array[i] === undefined
-                || array[i][j] === undefined) {
-                return;
-            } else if (array[i][j] === gene) {
-                count++;
-            }
-            if (count >= 3) {
-                repeatedGenes = true;
-            }
-        })
+  allGroups.forEach((group) => {
+    let count = 0
+    group.forEach(([i, j]) => {
+      if (array[i] === undefined || array[i][j] === undefined) {
+        return
+      } else if (array[i][j] === gene) {
+        count++
+      }
+      if (count >= 3) {
+        repeatedGenes = true
+      }
     })
+  })
 
-    return repeatedGenes;
+  return repeatedGenes
 }
 
-module.exports = hasMutation;
+module.exports = hasMutation
